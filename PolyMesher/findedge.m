3b57362491bd0caf35da43bfc315f9fe7898da7f
yuriyXiong <106143277+yuriyXiong@users.noreply.github.com>
2022-11-13 00:12:54 +0800

    Add files via upload

---
+++ PolyMesher/findedge.m
@@ -0,0 +1,42 @@
+function findedge(node,elem,varargin)
+%Findedge highlights edges
+
+% varargin: input interfaceData to draw a line on interface
+% Y.Xiong at 12.Nov.22
+
+hold on
+
+NT = size(elem,1);
+if ~iscell(elem) % transform to cell
+    elem = mat2cell(elem,ones(NT,1),length(elem(1,:)));
+end
+
+% -------- edge matrix -------
+shiftfun = @(verts) [verts(2:end),verts(1)];
+T1 = cellfun(shiftfun, elem, 'UniformOutput', false);
+v0 = horzcat(elem{:})'; % the starting points of edges
+v1 = horzcat(T1{:})'; % the ending points of edges
+totalEdge = sort([v0,v1],2);
+[i,j,s] = find(sparse(totalEdge(:,2),totalEdge(:,1),1));
+edge = [j,i];
+% bdEdge = edge(s==1,:);
+
+% ------- range ---------
+range = find(s==1); % boundary edges
+
+
+% ------ edge index ------
+if isempty(varargin)
+    midEdge = (node(edge(range,1),:)+node(edge(range,2),:))/2;
+    plot(midEdge(:,1),midEdge(:,2),'s','LineWidth',1,'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[0.6 0.5 0.8],'MarkerSize',20);
+    text(midEdge(:,1)-0.025,midEdge(:,2),int2str(range), ...
+    'FontSize',12,'FontWeight','bold','Color','k');
+else
+    interfaceEdgeIdx = varargin{1}.edge;
+    h = line([node(interfaceEdgeIdx(:,1),1)'; node(interfaceEdgeIdx(:,2),1)'],...
+                     [node(interfaceEdgeIdx(:,1),2)'; node(interfaceEdgeIdx(:,2),2)'],...
+                    'LineWidth',2,'Color','r');
+     set(h,'Linewidth',3);
+        
+end
